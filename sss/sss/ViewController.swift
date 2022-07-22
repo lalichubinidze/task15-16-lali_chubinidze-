04cc0324af456dde3c8e5ef8d45f0e9b88a8e613
lali <lalichubinidze.1999@gmail.com>
2022-07-22 13:59:02 +0400

    add file

---
+++ sss/sss/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  sss
+//
+//  Created by iveri gamezardashvili on 7/22/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    func akfb() {
+
+    }
+
+
+}
+
